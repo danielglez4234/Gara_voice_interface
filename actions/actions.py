ec2e6b942358424c4b118615f1b7e391015e3607
danielglez4234 <gnzlzdaniel.task@gmail.com>
2022-12-19 13:23:28 +0000

    add testing custom actions

--- actions/actions.py
+++ actions/actions.py
@@ -11,17 +11,69 @@ from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import UserUtteranceReverted
+
+from rasa_sdk.events import SlotSet
+from rasa_sdk.types import DomainDict
+from rasa_sdk.events import SessionStarted, ActionExecuted
 
 
-class ActionHelloWorld(Action):
+class SessionStart():
+    def name(self) -> Text:
+        return "action_session_start"
+
+    async def run(
+      self, dispatcher, tracker: Tracker, domain: Dict[Text, Any]
+    ) -> List[Dict[Text, Any]]:
+        metadata = tracker.get_slot("session_started_metadata")
+
+        # Do something with the metadata
+        print(metadata)
+
+        # the session should begin with a `session_started` event and an `action_listen`
+        # as a user message follows
+        return [SessionStarted(), ActionExecuted("action_listen")]
+
+
+class ActionDefaultFallback(Action):
+    """Executes the fallback action and goes back to the previous state
+    of the dialogue"""
 
     def name(self) -> Text:
-        return "action_hello_world"
+        return "action_nlu_fallback"
+
+    async def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(template="I couldnt understand you")
+
+        # Revert user message which led to fallback.
+        return [UserUtteranceReverted()]
+
+
+class ActionGreet(Action):
+
+    def name(self) -> Text:
+        return "action_greet"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
+        print(tracker)
         dispatcher.utter_message(text="Hello World!")
 
         return []
+
+class ActionPresentation(Action):
+    
+    def name(self) -> Text:
+        return "action_presentation"
+    
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message("it works")
